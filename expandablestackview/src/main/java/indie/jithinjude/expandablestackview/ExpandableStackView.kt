e4751423c0b9db360f27681f1ab00f2bf0360f7b
Jithin Jude <jithin.jude68@gmail.com>
2023-06-29 11:24:02 +0530

    Initial commit

---
+++ expandablestackview/src/main/java/indie/jithinjude/expandablestackview/ExpandableStackView.kt
@@ -0,0 +1,24 @@
+package indie.jithinjude.expandablestackview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+
+/**
+ * Created by <Jithin/Jude> on 29,June,2023
+ */
+class ExpandableStackView: FrameLayout {
+    constructor(context: Context) : this(context, null)
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet?,
+                defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    fun init() {
+        View.inflate(context, R.layout.layout_expandable_stack_view, this)
+    }
+}
