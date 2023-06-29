461c601ac78b269865a700614a9d666596d3f5e0
Jithin Jude <jithin.jude68@gmail.com>
2023-06-29 15:16:54 +0530

    code cleanup

--- expandablestackview/src/main/java/indie/jithinjude/dev/ExpandableStackView.kt
+++ expandablestackview/src/main/java/indie/jithinjude/dev/ExpandableStackView.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.widget.FrameLayout
 import androidx.viewpager2.widget.ViewPager2
 import indie.jithinjude.dev.databinding.LayoutExpandableStackViewBinding
-import java.lang.Math.abs
 
 
 /**
@@ -24,7 +23,6 @@ class ExpandableStackView : FrameLayout {
     }
 
     fun init() {
-//        View.inflate(context, R.layout.layout_expandable_stack_view, this)
         val binding =
             LayoutExpandableStackViewBinding.inflate(LayoutInflater.from(context), this, true)
 
@@ -36,43 +34,22 @@ class ExpandableStackView : FrameLayout {
         val adapter = ExpandableStackViewAdapter(stackItemList)
         binding.rvStackView.adapter = adapter
 
-        // You need to retain one page on each side so that the next and previous items are visible
         binding.rvStackView.offscreenPageLimit = 1
 
-// Add a PageTransformer that translates the next and previous items horizontally
-// towards the center of the screen, which makes them visible
         val nextItemVisiblePx = resources.getDimension(R.dimen.viewpager_next_item_visible)
         val currentItemHorizontalMarginPx =
             resources.getDimension(R.dimen.viewpager_current_item_horizontal_margin)
         val pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx
         val pageTransformer = ViewPager2.PageTransformer { page: View, position: Float ->
             page.translationX = -pageTranslationX * position
-            // Next line scales the item's height. You can remove it if you don't want this effect
-            page.scaleY = 1 - (0.25f * abs(position))
-            // If you want a fading effect uncomment the next line:
-            // page.alpha = 0.25f + (1 - abs(position))
+            page.scaleY = 1 - (0.25f * kotlin.math.abs(position))
         }
         binding.rvStackView.setPageTransformer(pageTransformer)
 
-// The ItemDecoration gives the current (centered) item horizontal margin so that
-// it doesn't occupy the whole screen width. Without it the items overlap
         val itemDecoration = ItemSpacingDecoration(
             context,
             R.dimen.viewpager_current_item_horizontal_margin
         )
         binding.rvStackView.addItemDecoration(itemDecoration)
-
-
-//        binding.rvStackView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-//
-//        val horizontalMargin = resources.getDimensionPixelSize(R.dimen.dimen_8dp)
-//        val adjacentVisibleSize = resources.getDimensionPixelSize(R.dimen.dimen_16dp)
-//        val spacingInPixels = resources.getDimensionPixelSize(R.dimen.spacing)
-//        val itemDecoration = ItemSpacingDecoration(horizontalMargin = spacingInPixels, adjacentVisibleSize = spacingInPixels)
-//        binding.rvStackView.addItemDecoration(itemDecoration)
-//
-//        val helper: SnapHelper = PagerSnapHelper()
-//        helper.attachToRecyclerView(binding.rvStackView)
-
     }
 }
