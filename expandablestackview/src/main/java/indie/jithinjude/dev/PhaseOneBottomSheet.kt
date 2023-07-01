953601eeefdb79dcea01caffa041a398ddd4a1ee
Jithin Jude <jithin.jude68@gmail.com>
2023-07-01 21:41:35 +0530

    rounded_corner_bottom_sheet

--- expandablestackview/src/main/java/indie/jithinjude/dev/PhaseOneBottomSheet.kt
+++ expandablestackview/src/main/java/indie/jithinjude/dev/PhaseOneBottomSheet.kt
@@ -1,10 +1,14 @@
 package indie.jithinjude.dev
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import indie.jithinjude.dev.databinding.BottomSheetPhaseOneBinding
 
@@ -15,6 +19,12 @@ import indie.jithinjude.dev.databinding.BottomSheetPhaseOneBinding
 class PhaseOneBottomSheet : BottomSheetDialogFragment() {
 
     lateinit var binding: BottomSheetPhaseOneBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.BottomSheetStyle)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,6 +33,20 @@ class PhaseOneBottomSheet : BottomSheetDialogFragment() {
         binding =
             BottomSheetPhaseOneBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = BottomSheetDialog(requireContext(), theme)
+        dialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        return dialog
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
     }
 
     companion object {
